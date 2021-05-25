e89b06fc224e972978e08e13b8fa97b53c66e54e
khanhhungvu1508 <69689114+khanhhungvu1508@users.noreply.github.com>
2021-05-25 16:00:12 +0700

    Add files via upload

---
+++ Project_4/mult/Mult.asm
@@ -0,0 +1,62 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+// Put your code here.
+
+// ------------------------------------------
+// Pseudocode: R2 = Mult(R0, R1)
+// while (1) do
+//      if (R0 no change and R1 no change)
+//          continue // No latch
+//      res := 0, i := 0
+//      while (i < R0) do
+//          res := res + R1
+//          i := i + 1
+//      end do
+//      R2 := res
+// end do
+// ------------------------------------------
+
+@res
+M = 0 // res = 0
+
+@i
+M = 0 // i = 0
+
+(LOOP)
+	@i
+	D = M // D = i
+	@R0
+	D = M - D // D = i - R0
+	@END
+	D; JEQ // if i = R0 goto END
+        
+        // If not, this mean i < R0
+	@R1
+	D = M // D = R1
+	@res
+	M = D + M // res := res + R1
+
+	@i
+	M = M + 1 // i := i + 1
+
+	@LOOP
+	0; JMP // goto LOOP
+
+(END)
+@res
+D = M // D = res
+@R2
+M = D // R2 = res
+
+(INFINITE_LOOP)
+@INFINITE_LOOP
+0; JMP 
